9603f8b797226fab78924afb30cf7d823301cc53
Jesus <kiriltimofeev1999@gmail.com>
2026-07-02 18:01:52 +0300

    Initial commit

--- elpa/vterm-20260626.1906/build/CMakeFiles/vterm-module.dir/utf8.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/vterm-module.dir/utf8.c.o: \
-  /home/jesus/.emacs.d/elpa/vterm-20260626.1906/utf8.c \
-  /home/jesus/.emacs.d/elpa/vterm-20260626.1906/utf8.h \
-  /usr/include/stdbool.h /usr/include/stddef.h /usr/include/sys/cdefs.h \
-  /usr/include/machine/cdefs.h /usr/include/sys/_null.h \
-  /usr/include/sys/_types.h /usr/include/machine/_types.h \
-  /usr/include/stdint.h
